e27e6850322249bc8f3894013baf29bd7f17c05d
LandonB5 <landon.b5@icloud.com>
2025-10-21 16:50:59 -0400

    Add tutor profile page and dummy data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Tutors from './pages/Tutors';
+import TutorProfile from './pages/TutorProfile';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/tutors" element={<Tutors />} />
+        <Route path="/tutor/:id" element={<TutorProfile />} />
       </Routes>
     </Router>
   );
